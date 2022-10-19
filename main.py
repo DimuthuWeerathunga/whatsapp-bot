4a3e86179e6e24fde42f4126ff85ee9101a39ca9
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2022-10-19 12:10:09 +0530

    finished bot

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ chat_xpath = "//*[@id=\"pane-side\"]/div[2]/div/div/div[11]"
 
 chat_name_el = driver.find_element(By.XPATH, chat_xpath)
 chat_name_el.click()
-pdb.set_trace()
 
 input("Hit enter to send the message")
 
 text_box_xpath = "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"
 send_msg_btn_xpath = "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button"
 
-for i in range():
+for i in range(10):
     text_box_el = driver.find_element(By.XPATH, text_box_xpath)
     response = requests.get("https://api.chucknorris.io/jokes/random")
     message = response.json()["value"]
